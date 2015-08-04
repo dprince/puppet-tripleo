278d4d58f0db71977c59a26d9b5e721a0e260cae
Dan Prince <dprince@redhat.com>
2015-08-04 08:24:53 -0400

    tripleo::noop: add support for more resources
    This patch adds the ability to set the following resource
    types to noop:
    
     -file_line
     -concat
     -staging_file
     -mongodb_conn_validator
    
    Additionally the following resources may be set to noop by using
    a custom noop provider. Each of these extends a base noop.rb
    provider base class that does nothing.
    
     -vs_bridge
     -mongodb_replset
     -rabbitmq_plugin
     -mysql_user
     -mysql_database
     -mysql_grant
    
    Change-Id: I318ff628565a5dcd995f9ff066660c42a5cc5a5e

--- manifests/noop.pp
+++ manifests/noop.pp
@@ -46,15 +46,65 @@
 #  Whether Cron resources should be noop.
 #  Defaults to true
 #
+# [*file_line*]
+#  Whether File_line resources should be noop.
+#  Defaults to true
+#
+# [*concat*]
+#  Whether Concat resources should be noop.
+#  Defaults to true
+#
+# [*staging_file*]
+#  Whether Staging::File resources should be noop.
+#  Defaults to true
+#
+# [*vs_bridge*]
+#  Whether Vs_bridge resources should be noop.
+#  Defaults to true
+#
+# [*mongodb_conn_validator*]
+#  Whether Mongodb_conn_validator resources should be noop.
+#  Defaults to true
+#
+# [*mongodb_replset*]
+#  Whether Mongodb_replset resources should be noop.
+#  Defaults to true
+#
+# [*rabbitmq_plugin*]
+#  Whether Rabbitmq_plugin resources should be noop.
+#  Defaults to true
+#
+# [*mysql_user*]
+#  Whether Mysql_user resources should be noop.
+#  Defaults to true
+#
+# [*mysql_database*]
+#  Whether Mysql_database resources should be noop.
+#  Defaults to true
+#
+# [*mysql_grant*]
+#  Whether Mysql_grant resources should be noop.
+#  Defaults to true
+#
 #
 class tripleo::noop (
-  $package = true,
-  $file = true,
-  $service = true,
-  $exec = true,
-  $user = true,
-  $group = true,
-  $cron = true,
+  $package                = true,
+  $file                   = true,
+  $service                = true,
+  $exec                   = true,
+  $user                   = true,
+  $group                  = true,
+  $cron                   = true,
+  $file_line              = true,
+  $concat                 = true,
+  $staging_file           = true,
+  $vs_bridge              = true,
+  $mongodb_conn_validator = true,
+  $mongodb_replset        = true,
+  $rabbitmq_plugin        = true,
+  $mysql_user             = true,
+  $mysql_database         = true,
+  $mysql_grant            = true,
 ) {
 
   Package <| |> { noop => $package}
@@ -65,4 +115,28 @@ class tripleo::noop (
   Group <| |> { noop => $group}
   Cron <| |> { noop => $cron}
 
+  File_line <| |> { noop => $file_line }
+  Concat <| |> { noop => $concat }
+  Staging::File<| |> { noop => $staging_file}
+  Mongodb_conn_validator<| |> { noop => $mongodb_conn_validator }
+
+  # custom noop providers
+  if $vs_bridge {
+    Vs_bridge <| |> { provider => noop }
+  }
+  if $mongodb_replset {
+    Mongodb_replset<| |> { provider => noop }
+  }
+  if $rabbitmq_plugin {
+    Rabbitmq_plugin<| |> { provider => noop }
+  }
+  if $mysql_user {
+    Mysql_user<| |> { provider => noop }
+  }
+  if $mysql_database {
+    Mysql_database<| |> { provider => noop }
+  }
+  if $mysql_grant {
+    Mysql_grant<| |> { provider => noop }
+  }
 }
